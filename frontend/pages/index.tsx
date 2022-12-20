e4a0246c204bba648cfcdac0e0cf507243b3eb06
astapi <jyunichi.k@gmail.com>
2022-12-20 14:11:25 +0900

    first commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>dapps入門</title>
+      </Head>
+      <div>hello</div>
+    </>
+  );
+}
